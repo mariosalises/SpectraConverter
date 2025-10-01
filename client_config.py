e080aebd2ee486a3d9be6fc21d4499066319d608
Mario <mesanchezalises@gmail.com>
2025-10-01 20:58:40 +0200

    Version 4.0 ready

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'SpectraConverter'
+    COMPANY_NAME = 'Quicap AI Innovations'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['https://api.github.com/repos/mariosalises/SpectraConverter/releases']
